08bfcda8ceea9139c7f7d88997241b1258d3549e
sudee404 <sudaise18@gmail.com>
2023-03-25 17:18:15 +0300

    flushed db

--- core_settings/settings.py
+++ core_settings/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG',False)
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS'),]
 
